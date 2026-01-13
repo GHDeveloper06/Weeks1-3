91717e263c419c7b7f6f8b3447c3d242f9c3d141
2006_V2\steev <costath@sheridancollege.ca>
2026-01-13 10:12:58 -0500

    added pulse

---
+++ Assets/Scripts/Pulse.cs
@@ -0,0 +1,29 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Pulse : MonoBehaviour
+{
+    public float t;
+    public AnimationCurve curve;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+  
+    // Update is called once per frame
+    void Update()
+    {
+        t += Time.deltaTime;
+        If(t > 1) {
+            t = 0;
+        }
+
+        float y = curve.Evaluate(t);
+        transform.localScale = Vector3.one * y;
+        
+        
+        
+    }
+}
