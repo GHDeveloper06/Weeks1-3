32cd7d76150de53938671989a53bb57bebc72f89
costath <costath@OA-S141-04>
2026-01-20 10:47:33 -0500

    Created list to cycle through sprites. Attempted to have items in list be deleted on mouse click but that is not working right now.

--- Assets/Scripts/SpriteChanger.cs
+++ Assets/Scripts/SpriteChanger.cs
@@ -1,33 +1,47 @@
+using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using System.Collections.Generic;
+using Unity.Collections;
 
 public class SpriteChanger : MonoBehaviour
 {
     public SpriteRenderer spriteRenderer;
     public Color Colour;
+    public List<Sprite> barrels;
+    public int randomNum;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //PickRandomColour();
+        //PickRandomColour
     }
 
     // Update is called once per frame
     void Update()
     {
-        //if (Keyboard.current.anyKey.wasPressedThisFrame) {
-        //    //PickRandomColour();
-        //}
+       if (Keyboard.current.anyKey.wasPressedThisFrame == true) {
+            //PickRandomColour();
+            Debug.Log("Try to change the sprite renderer");
+            if (barrels.Count > 0)
+            {
+                pickRandomSprite();
+            }
+       }
         
 
         //get the mouse posistion
         Vector2 mousPos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         // is it over the shape
-        if (spriteRenderer.bounds.Contains(mousPos))
+        if (spriteRenderer.bounds.Contains(mousPos)==true)
         {
             spriteRenderer.color = Colour;
         }
         else {
             Colour = Color.grey;
+        }
+        if (Mouse.current.leftButton.wasPressedThisFrame == true && barrels.Count > 0) 
+        {
+            barrels.RemoveAt(0);
         }
         //y set the color with our Colour variable
         //n set the color to hwhite
@@ -37,7 +51,15 @@ public class SpriteChanger : MonoBehaviour
     //{
     //    spriteRenderer.color = Random.ColorHSV();
     //}
-    void changetoSelectedColour() { 
-    
+    //void changetoSelectedColour() { 
+
+    //}
+    void pickRandomSprite()
+    {
+        //spriteRenderer.sprite = mySprite; 
+
+        //pick a random number
+        randomNum = Random.Range(0, barrels.Count);
+        spriteRenderer.sprite = barrels[randomNum];
     }
 }
