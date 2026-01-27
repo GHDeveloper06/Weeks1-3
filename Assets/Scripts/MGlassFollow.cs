1af3c97148b6429048d29b6ce78b2ce9eb04470e
2006_V2\steev <costath@sheridancollege.ca>
2026-01-27 17:26:40 -0500

    Deleted files Not needed and updated MGlassFollow to apply an animation curve

--- Assets/Scripts/MGlassFollow.cs
+++ Assets/Scripts/MGlassFollow.cs
@@ -3,6 +3,12 @@ using UnityEngine.InputSystem;
 
 public class MGlassFollow : MonoBehaviour
 {
+    //count variable
+    float t;
+    //reference variable for Animation Curve
+    public AnimationCurve ACpulse;
+
+    //get game camera
     public Camera gameCamera;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,9 +19,22 @@ public class MGlassFollow : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        float c = ACpulse.Evaluate(t);
+        //go through Animation Curve every frame
+        t += Time.deltaTime;
+        //reset after 1 second
+        if (t > 1f) 
+        {
+            t = 0.1f;
+        }
+        //change scale based off of animation curve position
+        transform.localScale = Vector3.one * c;
+
         //create vector2 to find current mouse position
         Vector2 mousePos = Mouse.current.position.ReadValue();
+        //convert screen space to world space using Camera in hierarchy as reference
         Vector2 mpConvert = gameCamera.ScreenToWorldPoint(mousePos);
+        //change position based off where the cursor is from the game camera view
         transform.position = mpConvert;
     }
 }
